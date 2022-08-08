d3a1d60e1d5885a2b2795dc1061f25313f166155
logi-lab <logeshthemaster@gmail.com>
2022-08-08 19:55:03 +0530

    @LOGI-LAB

---
+++ Logi/Plugins/Tools/music.py
@@ -0,0 +1,6 @@
+__mod_name__ = "Music"
+
+__help__ = """ *Now Donwload and hear/watch song on telegram
+ ‣ `/mplay on my way`*:* this command is for music 
+ ‣ `/mstats born alone die alone` *:* download video from youtube
+"""
---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m Logi
+ps:scale worker=1
